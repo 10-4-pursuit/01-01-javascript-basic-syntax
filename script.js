fe330734e9561f3602f2532ae8a011cd8fc7c336
athomas227 <andrewthomas@pursuit.org>
2023-12-04 19:31:19 -0500

    finished exercise

---
+++ script.js
@@ -0,0 +1,13 @@
+const greeting = "Hello, world!";
+console.log(greeting);
+
+const number = 23;
+const anotherNumber= 8;
+console.log(number + anotherNumber);
+
+const name= "Bob";
+if (name === "Alice") {
+    console.log("Hello Alice!");
+} else {
+    console.log("You are not Alice");
+}
