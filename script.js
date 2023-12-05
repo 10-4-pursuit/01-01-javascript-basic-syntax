f1a42bbac5c473b6e9e6a5d51e2b9b28c28284c7
DarnishaTurner <darnishaturner@pursuit.org>
2023-12-05 18:36:59 -0500

    Darnishas Basic Syntax

---
+++ script.js
@@ -0,0 +1,14 @@
+const greeting = `Hello, world!`;
+console.log(greeting);
+
+const number = 3;
+const anotherNumber = 7;
+const sum = number + anotherNumber;
+console.log(sum);
+
+const name = `Alice`;
+if(name === `Alice`) {
+    console.log(`Hello Alice!`);
+} else {
+    console.log(`You are not Alice`);
+}
