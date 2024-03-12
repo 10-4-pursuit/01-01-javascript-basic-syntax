7050a581b21ab61aa2cef9fcd6c1f1e3ec143567
dttrue <danieltorres@pursuit.org>
2024-03-12 18:57:22 -0400

    Daniels JS Basic-syntax

---
+++ script.js
@@ -0,0 +1,22 @@
+const greeting = "Hello, World!"
+
+const number = 111
+const anotherNuber = 1000
+
+sum = number + anotherNuber
+
+
+const greetingName = (name) => {
+if(name === "Alice"){
+    return "Hello Alice"
+}else{
+    return "Your not Alice"
+}
+}
+
+
+
+
+console.log(sum)
+console.log(greeting)
+console.log(greetingName("Alice"))
